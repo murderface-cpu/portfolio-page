59f0f9aa98aaee4b03a3cb0ff063fb52510e452b
Bruce Kiptoo <brckiptoo@gmail.com>
2026-06-10 09:50:19 -0400

    Change basepath from '/myapp' to '/'

--- src/router.tsx
+++ src/router.tsx
@@ -7,7 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
-    basepath: "/myapp",
+    basepath: "/",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
